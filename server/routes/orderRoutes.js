46d57ad9ab502e8eb26384feffa46cbb3108251f
alikmanukyan080-dotcom <alikmanukyan080@gmail.com>
2026-09-05 09:18:28 +0400

    Update orderRoutes.js

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -5,17 +5,18 @@ const {
   getOrderById,
   getOrderByNumber,
   updateOrderStatus,
-  getDashboardStats
+  getDashboardStats,
+  deleteOrder
 } = require('../controllers/orderController');
 const { protect } = require('../middleware/auth');
-
 const router = express.Router();
-
+ 
 router.post('/', createOrder);
 router.get('/', protect, getOrders);
 router.get('/stats/dashboard', protect, getDashboardStats);
 router.get('/track/:number', getOrderByNumber);
 router.get('/:id', protect, getOrderById);
 router.put('/:id/status', protect, updateOrderStatus);
-
+router.delete('/:id', protect, deleteOrder);
+ 
 module.exports = router;
